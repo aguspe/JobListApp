352f54f90be569ed60e917e3a7fafeeba9b96b5a
Aguspe <pequesitos@hotmail.es>
2019-04-20 17:24:58 +0200

    React views updated

---
+++ src/views/views/src/App.js
@@ -0,0 +1,172 @@
+
+// /client/App.js
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+    // initialize our state
+    state = {
+        data: [],
+        title: null,
+        description: null,
+        answers: null,
+    };
+
+    // when component mounts, first thing it does is fetch all existing data in our db
+    // then we incorporate a polling logic so that we can easily see if our db has
+    // changed and implement those changes into our UI
+    componentDidMount() {
+        this.getDataFromDb();
+        if (!this.state.intervalIsSet) {
+            let interval = setInterval(this.getDataFromDb, 1000);
+            this.setState({ intervalIsSet: interval });
+        }
+    }
+
+    // never let a process live forever
+    // always kill a process everytime we are done using it
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+            clearInterval(this.state.intervalIsSet);
+            this.setState({ intervalIsSet: null });
+        }
+    }
+
+    // just a note, here, in the front end, we use the id key of our data object
+    // in order to identify which we want to Update or delete.
+    // for our back end, we use the object id assigned by MongoDB to modify
+    // data base entries
+
+    // our first get method that uses our backend api to
+    // fetch data from our data base
+    getDataFromDb = () => {
+        fetch("http://localhost:5000/api/questions")
+            .then(data => data.json())
+            .then(res => this.setState({ data: res.data }));
+    };
+
+    // our put method that uses our backend api
+    // to create new query into our data base
+    putDataToDB = (title,description) => {
+        let currentIds = this.state.data.map(data => data.id);
+        let idToBeAdded = 0;
+        while (currentIds.includes(idToBeAdded)) {
+            ++idToBeAdded;
+        }
+
+        axios.post("http://localhost:5000/api/questions", {
+            id: idToBeAdded,
+            title: title,
+            description: description
+        });
+    };
+
+
+    // our delete method that uses our backend api
+    // to remove existing database information
+    // deleteFromDB = idTodelete => {
+    //     let objIdToDelete = null;
+    //     this.state.data.forEach(dat => {
+    //         if (dat.id == idTodelete) {
+    //             objIdToDelete = dat._id;
+    //         }
+    //     });
+    //
+    //     axios.delete("http://localhost:3001/api/deleteData", {
+    //         data: {
+    //             id: objIdToDelete
+    //         }
+    //     });
+    // };
+
+
+    // our update method that uses our backend api
+    // to overwrite existing data base information
+    // updateDB = (idToUpdate, updateToApply) => {
+    //     let objIdToUpdate = null;
+    //     this.state.data.forEach(dat => {
+    //         if (dat.id == idToUpdate) {
+    //             objIdToUpdate = dat._id;
+    //         }
+    //     });
+    //
+    //     axios.post("http://localhost:3001/api/updateData", {
+    //         id: objIdToUpdate,
+    //         update: { message: updateToApply }
+    //     });
+    // };
+
+
+    // here is our UI
+    // it is easy to understand their functions when you
+    // see them render into our screen
+    render() {
+        const { data } = this.state;
+        return (
+            <div>
+                <ul>
+                    {data.length <= 0
+                        ? "NO DB ENTRIES YET"
+                        : data.map(dat => (
+                            <li style={{ padding: "10px" }} key={data.message}>
+                                <span style={{ color: "gray" }}> title: </span> {dat.title} <br />
+                                <span style={{ color: "gray" }}> description: </span> {dat.description} <br />
+                                {dat.message}
+                            </li>
+                        ))}
+                </ul>
+                <div style={{ padding: "10px" }}>
+                    <input
+                    type="text"
+                    onChange={e => this.setState({ title: e.target.value })}
+                    placeholder="add something in the database"
+                    style={{ width: "200px" }}
+                />
+                    <input
+                        type="text"
+                        onChange={e => this.setState({ description: e.target.value })}
+                        placeholder="add something in the database"
+                        style={{ width: "200px" }}
+                    />
+                    <button onClick={() => this.putDataToDB(this.state.title, this.state.description )}>
+                        ADD
+                    </button>
+                </div>
+                {/*<div style={{ padding: "10px" }}>*/}
+                {/*    <input*/}
+                {/*        type="text"*/}
+                {/*        style={{ width: "200px" }}*/}
+                {/*        onChange={e => this.setState({ idToDelete: e.target.value })}*/}
+                {/*        placeholder="put id of item to delete here"*/}
+                {/*    />*/}
+                {/*    <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>*/}
+                {/*        DELETE*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+                {/*<div style={{ padding: "10px" }}>*/}
+                {/*    <input*/}
+                {/*        type="text"*/}
+                {/*        style={{ width: "200px" }}*/}
+                {/*        onChange={e => this.setState({ idToUpdate: e.target.value })}*/}
+                {/*        placeholder="id of item to update here"*/}
+                {/*    />*/}
+                {/*    <input*/}
+                {/*        type="text"*/}
+                {/*        style={{ width: "200px" }}*/}
+                {/*        onChange={e => this.setState({ updateToApply: e.target.value })}*/}
+                {/*        placeholder="put new value of the item here"*/}
+                {/*    />*/}
+                {/*    <button*/}
+                {/*        onClick={() =>*/}
+                {/*            this.updateDB(this.state.idToUpdate, this.state.updateToApply)*/}
+                {/*        }*/}
+                {/*    >*/}
+                {/*        UPDATE*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+            </div>
+        );
+    }
+}
+
+export default App;
