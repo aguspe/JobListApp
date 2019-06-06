248f8432a466dcd5f316300308bb50cf838c2acd
Aguspe <pequesitos@hotmail.es>
2019-06-06 19:58:27 +0200

    Ready for deployment

--- src/views/views/src/Tests/LoginView.test.js
+++ src/views/views/src/Tests/LoginView.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import LoginView from "../User/LoginView";
-import {render} from "@testing-library/react";
+import {cleanup, render,  fireEvent} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
+import HomeView from "../General/HomeView";
 
 describe('Check for text in header', () => {
     it('should contain the proper text', () => {
@@ -10,3 +11,21 @@ describe('Check for text in header', () => {
     });
 });
 
+afterEach(cleanup);
+describe('Login button clicked', () => {
+    it('should click on text', () => {
+        const { getByText } = render(<BrowserRouter><LoginView name="Submit" /></BrowserRouter>);
+        const textEl = getByText('Submit');
+        fireEvent.click(textEl)
+    })
+});
+
+afterEach(cleanup);
+describe('Test that there are no changes on re rendering', () => {
+    it('should contains name with prop change', () => {
+        const { getByText, rerender } = render(<BrowserRouter><LoginView name="Submit" /></BrowserRouter>);
+        getByText('Submit');
+        rerender(<BrowserRouter><LoginView name="Submit" /></BrowserRouter>);
+        getByText('Submit')
+    })
+});
