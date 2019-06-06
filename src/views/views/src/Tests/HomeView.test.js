3028fa627e9c4b3d4be208a65544ccd23324895b
Aguspe <pequesitos@hotmail.es>
2019-06-06 19:51:14 +0200

    Ready for deployment

--- src/views/views/src/Tests/HomeView.test.js
+++ src/views/views/src/Tests/HomeView.test.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import HomeView from "../General/HomeView";
-import {render} from "@testing-library/react";
+import {cleanup, render, fireEvent} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
 
 describe('Check for link text', () => {
     it('should contain the proper text', () => {
-        const {getByText} = render(<HomeView/>);
+        const {getByText} = render(<BrowserRouter><HomeView/></BrowserRouter>);
        getByText('Login as a company before posting');
     });
 });
 
+afterEach(cleanup);
+describe('Login button clicked', () => {
+    it('should click on text', () => {
+        const { getByText } = render(<BrowserRouter><HomeView name="Search for a job!" /></BrowserRouter>);
+        const textEl = getByText('Search for a job!');
+        fireEvent.click(textEl)
+    })
+});
