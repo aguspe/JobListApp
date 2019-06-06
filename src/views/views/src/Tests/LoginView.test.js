497740344c359c6af662cb1f1f540df812bcc92b
Aguspe <pequesitos@hotmail.es>
2019-06-06 19:35:30 +0200

    Ready for deployment

--- src/views/views/src/Tests/LoginView.test.js
+++ src/views/views/src/Tests/LoginView.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import LoginView from "../User/LoginView";
 import {render} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
 
 describe('Check for text in header', () => {
     it('should contain the proper text', () => {
-        const {getByText} = render(<LoginView/>);
+        const {getByText} = render(<BrowserRouter><LoginView/></BrowserRouter>);
         getByText('Login as a company!');
     });
 });
